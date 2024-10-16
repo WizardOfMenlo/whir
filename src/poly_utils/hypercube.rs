11f87ce1523dbdd29468c43e0d2594f6a7249ff4
Gottfried Herold <gottfried.herold@rub.de>
2024-10-16 08:05:15 +0200

    poly_utils/hypercube.rs: Add documentation; add check for number of variables in BinaryHypercube. The current implementation asserts num_variables < 32 or 64 (platform-dependent).

--- src/poly_utils/hypercube.rs
+++ src/poly_utils/hypercube.rs
@@ -1,13 +1,22 @@
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+
+// TODO (Gotti): Should pos rather be a u64? usize is platform-dependent, giving a platform-dependent limit on the number of variables.
+// num_variables may be smaller as well.
+
+/// point on the binary hypercube {0,1}^n for some n.
+/// 
+/// The point is encoded via the bits of a usize and we do not store n.
 pub struct BinaryHypercubePoint(pub usize);
 
+// BinaryHypercube is an Iterator that is used to range over the points of {0,1}^n, where n == `num_variables`
 pub struct BinaryHypercube {
-    pos: usize,
-    num_variables: usize,
+    pos: usize,  // current position, encoded via the bits of pos
+    num_variables: usize, // dimension of the hypercube
 }
 
 impl BinaryHypercube {
     pub fn new(num_variables: usize) -> Self {
+        debug_assert!(num_variables < usize::BITS as usize);
         BinaryHypercube {
             pos: 0,
             num_variables,
