b8dd4d2a4871a86a7cac69dd1797b4a2ff1c5f6e
dmpierre <pdaixmoreux@gmail.com>
2024-10-21 11:24:56 +0200

    feat: implement evm friendly fiat shamir

--- src/fs_utils.rs
+++ src/fs_utils.rs
@@ -10,15 +10,26 @@ use nimue::{
 
 /// Fiat shamir, for the EVM
 /// Prototype implementation
+#[derive(Clone)]
 pub struct EVMFs<F: Field> {
     _p: PhantomData<F>,
-    transcript: Vec<u8>,
+    transcript: Vec<u8>, // holds full script
+    state: Vec<u8>,      // holds values to be hashed for next challenge derivation
 }
 
 impl<F: Field> EVMFs<F> {
     pub fn new() -> Self {
         Self {
             transcript: vec![],
+            state: vec![],
+            _p: PhantomData,
+        }
+    }
+
+    pub fn to_arthur(&self) -> Self {
+        Self {
+            transcript: self.transcript.clone(),
+            state: vec![],
             _p: PhantomData,
         }
     }
@@ -30,8 +41,8 @@ impl<F: Field> EVMFs<F> {
         Ok(bytes)
     }
 
-    fn bytes_to_scalar(bytes: &[u8]) -> F {
-        F::from_base_prime_field(F::BasePrimeField::from_be_bytes_mod_order(bytes))
+    pub fn bytes_to_scalar(bytes: &[u8]) -> F {
+        F::from_base_prime_field(F::BasePrimeField::from_be_bytes_mod_order(&bytes))
     }
 
     fn keccak(left: &[u8], right: &[u8]) -> [u8; 32] {
@@ -40,14 +51,14 @@ impl<F: Field> EVMFs<F> {
         keccak.finalize().into()
     }
 
-    /// Derive `n` challenges from provided initial `init` value
-    /// h(h( ... h(init, 0), n-2), n-1)
-    fn derive_challenges(init: &[u8], n: u32) -> Vec<F> {
-        let mut challenges =
-            Vec::with_capacity(n.try_into().expect("are you on a 16 bits machine?"));
+    /// Arthur/Merlin
+    /// Squeezes `n` scalar elements using current `state`
+    /// TODO: handle various n: u32, u64; should align with EVM implementation
+    pub fn squeeze_scalars(&mut self, n: usize) -> Vec<F> {
+        let mut challenges = Vec::with_capacity(n);
+        let n: u32 = n.try_into().unwrap();
 
-        // push first challenge
-        let mut challenge_bytes = Self::keccak(init, &0_u32.to_be_bytes());
+        let mut challenge_bytes = Self::keccak(self.state.as_slice(), &0_u32.to_be_bytes());
         challenges.push(Self::bytes_to_scalar(&challenge_bytes));
 
         // push remaining challenges
@@ -56,64 +67,87 @@ impl<F: Field> EVMFs<F> {
             challenges.push(Self::bytes_to_scalar(&challenge_bytes));
         }
 
+        self.state = vec![];
         challenges
     }
 
-    /// Derive `n` challenges using `scalars.len()` provided values.
-    /// Returns Result, due to possible serialization errors for F to bytes serialization
-    /// TODO: be `usize` friendly
-    pub fn derive_challenges_from_scalars(scalars: &[F], n: usize) -> Result<Vec<F>, ProofError> {
-        let n: u32 = n
-            .try_into()
-            .expect("EVMFs outputs at most 2^32 - 1 challenges");
-        let value = scalars
-            .iter()
-            .map(Self::scalar_to_bytes)
-            .collect::<Result<Vec<[u8; 32]>, SerializationError>>()?
-            .concat();
+    /// Arthur/Merlin
+    /// Squeezes `n` bytes arrays using current `state`
+    /// TODO: handle various n: u32, u64; should align with EVM implementation
+    pub fn squeeze_bytes(&mut self, n: usize) -> Vec<[u8; 32]> {
+        let mut challenges = Vec::with_capacity(n);
 
-        Ok(Self::derive_challenges(&value, n))
+        let n: u32 = n.try_into().unwrap();
+        let mut challenge_bytes = Self::keccak(self.state.as_slice(), &0_u32.to_be_bytes());
+        challenges.push(challenge_bytes);
+
+        // push remaining challenges
+        for i in 1..n {
+            challenge_bytes = Self::keccak(&challenge_bytes, &i.to_be_bytes());
+            challenges.push(challenge_bytes);
+        }
+
+        self.state = vec![];
+        challenges
     }
 
-    /// Derive `n` challenges using provided bytes
-    /// TODO: be `usize` friendly
-    pub fn derive_challenges_from_bytes(bytes: &[u8], n: usize) -> Vec<F> {
-        let n: u32 = n
-            .try_into()
-            .expect("EVMFs outputs at most 2^32 - 1 challenges");
-        Self::derive_challenges(bytes, n)
+    pub fn transcript(&self) -> Vec<u8> {
+        self.transcript.to_vec()
     }
 
-    pub fn transcript(self) -> Vec<u8> {
-        self.transcript
+    pub fn state(&self) -> Vec<u8> {
+        self.state.to_vec()
     }
 
     fn push_to_transcript(&mut self, bytes: &[u8]) {
         let mut pushed = bytes.to_vec();
-        self.transcript.append(&mut pushed);
+        self.transcript.append(&mut pushed.clone());
+        self.state.append(&mut pushed);
     }
 
-    /// Pops `n` scalar elements from the transcript, FIFO
-    /// Assumes each scalar is in 32 bytes
-    pub fn pop(&mut self, n: usize) -> Vec<F> {
-        self.transcript
-            .drain(..n * 32)
-            .as_slice()
-            .chunks(32)
-            .map(Self::bytes_to_scalar)
-            .collect::<Vec<F>>()
+    /// Arthur
+    /// Removes `n` bytes elements from the transcript, appends them to the state and returns them
+    pub fn next_bytes(&mut self, n: usize) -> Vec<u8> {
+        let mut sliced = self.transcript[..n].to_vec();
+        self.state.append(&mut sliced.clone());
+        self.transcript = self.transcript[n..].to_vec();
+        sliced.reverse(); // return them in same state before absorption
+        sliced
     }
 
-    pub fn push_bytes(&mut self, bytes: &[u8]) {
+    /// Arthur
+    /// Removes `n` scalar elements from the transcript, appends them to the state and returns them
+    /// Assumes user is using scalars of 32 bytes each
+    pub fn next_scalars(&mut self, n: usize) -> Vec<F> {
+        let sliced = self.transcript[..n * 32].to_vec();
+        let scalar_bytes = sliced.chunks(32);
+        let mut scalars = vec![];
+        for bytes in scalar_bytes {
+            self.state.append(&mut bytes.to_vec().clone()); // was absorbed after reverse, append as given from
+                                                            // transcript
+            scalars.push(Self::bytes_to_scalar(&bytes));
+        }
+        self.transcript = self.transcript[n * 32..].to_vec();
+        scalars
+    }
+
+    /// Merlin
+    pub fn absorb_bytes(&mut self, bytes: &[u8]) {
         assert!(bytes.len() == 32, "EVMFs expects 32 bytes arrays");
-        Self::push_to_transcript(self, bytes);
+        // when serialized uncompressed, F is serialized in be
+        // so, assume bytes are be, hence need to reverse them
+        let mut le_bytes = bytes.to_vec();
+        le_bytes.reverse();
+        self.push_to_transcript(&le_bytes);
     }
 
-    /// Appends provided scalars to the transcript
+    /// Merlin
+    /// "Absorbs" provided scalars: appends them to the state and the transcript
     /// Returns Result, due to possible serialization errors for F to bytes serialization
-    pub fn push_scalars(&mut self, scalars: &[F]) -> Result<(), ProofError> {
+    pub fn absorb_scalars(&mut self, scalars: &[F]) -> Result<(), ProofError> {
         for scalar in scalars {
-            Self::push_to_transcript(self, &Self::scalar_to_bytes(scalar)?);
+            let bytes = Self::scalar_to_bytes(scalar)?;
+            self.push_to_transcript(&bytes);
         }
         Ok(())
     }
@@ -159,28 +193,47 @@ where
 pub mod tests {
     use super::EVMFs;
     use crate::crypto::fields::FieldBn256;
-    use ark_serialize::CanonicalSerialize;
     pub type F = FieldBn256;
 
     #[test]
     fn test_evm_fs() {
-        let scalars =
-            EVMFs::derive_challenges_from_scalars(&[F::from(42), F::from(24), F::from(42)], 1)
-                .unwrap();
-        assert_eq!(scalars.len(), 1);
-        let scalars =
-            EVMFs::derive_challenges_from_scalars(&[F::from(42), F::from(24), F::from(42)], 5)
-                .unwrap();
-        assert_eq!(scalars.len(), 5);
-    }
-
-    #[test]
-    fn test_transcript() {
-        let elements = [F::from(42), F::from(24), F::from(34)];
-        let mut bytes = [0_u8; 32];
-        let _ = F::from(2).serialize_compressed(&mut bytes[..]);
         let mut evmfs = EVMFs::new();
-        evmfs.push_scalars(&elements).unwrap();
-        evmfs.push_bytes(&bytes);
+        let _ = evmfs
+            .absorb_scalars(&[F::from(42), F::from(24), F::from(42)])
+            .unwrap();
+        let scalars = evmfs.squeeze_scalars(1);
+        println!("{}", scalars[0]);
+        assert_eq!(evmfs.state.len(), 0);
+
+        let _ = evmfs
+            .absorb_scalars(&[F::from(42), F::from(24), F::from(42)])
+            .unwrap();
+        let scalars = evmfs.squeeze_scalars(5);
+        for scalar in scalars {
+            println!("{}", scalar);
+        }
+
+        assert_eq!(evmfs.transcript().len(), 192); // 6 field elements * 32
+        assert_eq!(evmfs.state.len(), 0);
+
+        // New transcript
+        let mut evmfs = EVMFs::<F>::new();
+        let absorbed_bytes = Vec::from_iter(0_u8..32_u8);
+        let absorbed_scalars_1 = [F::from(1), F::from(2)];
+        let _ = evmfs.absorb_bytes(&absorbed_bytes);
+        let squeezed_1 = evmfs.squeeze_scalars(1)[0];
+        let _ = evmfs.absorb_scalars(&absorbed_scalars_1);
+        let squeezed_2 = evmfs.squeeze_scalars(1)[0];
+        assert_eq!(evmfs.transcript().len(), 96);
+        assert_eq!(evmfs.state.len(), 0);
+
+        // Resets state, keep transcript only
+        let mut arthur_evmfs = evmfs.to_arthur();
+        let _ = arthur_evmfs.next_bytes(32);
+        assert_eq!(arthur_evmfs.squeeze_scalars(1)[0], squeezed_1);
+        let next_scalars = arthur_evmfs.next_scalars(2);
+        assert_eq!(next_scalars[0], absorbed_scalars_1[0]);
+        assert_eq!(next_scalars[1], absorbed_scalars_1[1]);
+        assert_eq!(arthur_evmfs.squeeze_scalars(1)[0], squeezed_2);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod cmdline_utils;
 pub mod crypto; // Crypto utils
 pub mod domain; // Domain that we are evaluating over
+pub mod evm_utils; // Utilities for verifying whir on the evm
 pub mod fs_utils;
 pub mod ntt;
 pub mod parameters;
