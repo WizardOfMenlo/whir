9290aeaaaf7bf0cd8e6ece265faea72c3c8e4578
Thomas Coratger <thomas.coratger@gmail.com>
2025-03-21 21:14:27 +0100

    two_inv modif

--- src/parameters.rs
+++ src/parameters.rs
@@ -116,6 +116,7 @@ impl FoldType {
                 // w^2 * <w^(N/16)>, and so on. Until w^(N/16-1) * <w^(N/16)>.
                 let coset_generator_inv =
                     domain_gen_inv.pow([(domain_size / coset_domain_size) as u64]);
+                let two_inv = F::from(2).inverse().unwrap();
                 stir_evaluations.extend(stir_challenges_indexes.iter().zip(answers).map(
                     |(index, answers)| {
                         // The coset is w^index * <w_coset_generator>
@@ -132,7 +133,7 @@ impl FoldType {
                             &round_state.folding_randomness.0,
                             coset_offset_inv,
                             coset_generator_inv,
-                            F::from(2).inverse().unwrap(),
+                            two_inv,
                             folding_factor.at_round(round_state.round),
                         )
                     },
