63535603dca337adb646ff8a8d0541e91dbc4af0
Remco Bloemen <remco@wicked.ventures>
2025-05-19 14:23:28 +0200

    Factor out verify_stir_challenges

--- src/whir/verifier.rs
+++ src/whir/verifier.rs
@@ -38,6 +38,18 @@ where
     _state: PhantomData<VerifierState>,
 }
 
+// TODO: Merge these into RoundConfig
+pub struct StirChallengParams<F> {
+    round_index: usize,
+    domain_size: usize,
+    num_variables: usize,
+    folding_factor: usize,
+    num_queries: usize,
+    pow_bits: f64,
+    domain_gen_inv: F,
+    exp_domain_gen: F,
+}
+
 impl<'a, F, MerkleConfig, PowStrategy, VerifierState>
     Verifier<'a, F, MerkleConfig, PowStrategy, VerifierState>
 where
@@ -76,24 +88,15 @@ where
         let mut folding_randomness;
         let initial_combination_randomness;
         if self.params.initial_statement {
-            // Collect initial constraints
+            // Combine OODS and statement constraints to claimed_sum
             let constraints: Vec<_> = parsed_commitment
                 .oods_constraints()
                 .into_iter()
                 .chain(statement.constraints.iter().cloned())
                 .collect();
-
-            // Derive combination randomness and first sumcheck polynomial
-            let [combination_randomness_gen] = verifier_state.challenge_scalars()?;
             initial_combination_randomness =
-                expand_randomness(combination_randomness_gen, constraints.len());
-
-            // Compute initial sum
-            claimed_sum = constraints
-                .iter()
-                .zip(&initial_combination_randomness)
-                .map(|(c, rand)| c.sum * rand)
-                .sum();
+                self.combine_constraints(verifier_state, &mut claimed_sum, &constraints)?;
+            constraints_at_round.push(constraints);
 
             // Initial sumcheck
             folding_randomness = self.verify_sumcheck_rounds(
@@ -102,9 +105,6 @@ where
                 self.params.folding_factor.at_round(0),
                 self.params.starting_folding_pow_bits,
             )?;
-
-            // Store constraints for final evaluation
-            constraints_at_round.push(constraints);
         } else {
             assert_eq!(parsed_commitment.ood_points.len(), 0);
             assert!(statement.constraints.is_empty());
@@ -131,65 +131,45 @@ where
         let mut rounds = vec![];
 
         for round_index in 0..self.params.n_rounds() {
+            // Comput round parameters
+            // TODO: Move to RoundConfig
             let round_params = &self.params.round_parameters[round_index];
             let folding_factor = self.params.folding_factor.at_round(round_index);
+            let params = StirChallengParams {
+                round_index,
+                domain_size,
+                num_variables,
+                num_queries: round_params.num_queries,
+                folding_factor,
+                pow_bits: round_params.pow_bits,
+                domain_gen_inv,
+                exp_domain_gen,
+            };
 
+            // Receive commitment to the folded polynomial (likely encoded at higher rate)
             let new_commitment = ParsedCommitment::<F, MerkleConfig::InnerDigest>::parse(
                 verifier_state,
                 num_variables,
                 round_params.ood_samples,
             )?;
 
-            let stir_challenges_indexes = get_challenge_stir_queries(
-                domain_size,
-                folding_factor,
-                round_params.num_queries,
+            // Verify in-domain challenges on the previous commitment.
+            let (stir_constraints, stir_challenges_points) = self.verify_stir_challenges(
                 verifier_state,
-            )?;
-
-            let answers = self.verify_merkle_proof(
-                verifier_state,
-                &prev_commitment.root,
-                &stir_challenges_indexes,
-            )?;
-
-            self.verify_proof_of_work(verifier_state, round_params.pow_bits)?;
-
-            // Compute STIR Constraints
-            let folds = self.params.fold_optimisation.stir_evaluations_verifier(
-                self.params,
-                round_index,
-                domain_gen_inv,
-                &stir_challenges_indexes,
+                &params,
+                &prev_commitment,
                 &folding_randomness,
-                &answers,
-            );
-            let stir_constraints = stir_challenges_indexes
-                .iter()
-                .map(|&index| exp_domain_gen.pow([index as u64]))
-                .zip(&folds)
-                .map(|(point, &value)| Constraint {
-                    weights: Weights::univariate(point, num_variables),
-                    sum: value,
-                    deferred: false,
-                });
+            )?;
 
-            // Add OODS and STIR constraints to claimed_sum
+            // Add out-of-domain and in-domain constraints to claimed_sum
             let constraints: Vec<Constraint<F>> = new_commitment
                 .oods_constraints()
                 .into_iter()
-                .chain(stir_constraints)
+                .chain(stir_constraints.into_iter())
                 .collect();
-            let [combination_randomness_gen] = verifier_state.challenge_scalars()?;
-            let combination_randomness = expand_randomness(
-                combination_randomness_gen,
-                stir_challenges_indexes.len() + round_params.ood_samples,
-            );
-            claimed_sum += constraints
-                .iter()
-                .zip(&combination_randomness)
-                .map(|(c, rand)| c.sum * rand)
-                .sum::<F>();
+            let combination_randomness =
+                self.combine_constraints(verifier_state, &mut claimed_sum, &constraints)?;
+            constraints_at_round.push(constraints);
 
             let new_folding_randomness = self.verify_sumcheck_rounds(
                 verifier_state,
@@ -201,13 +181,9 @@ where
             rounds.push(ParsedRound {
                 folding_randomness,
                 ood_points: new_commitment.ood_points.clone(),
-                stir_challenges_points: stir_challenges_indexes
-                    .iter()
-                    .map(|index| exp_domain_gen.pow([*index as u64]))
-                    .collect(),
+                stir_challenges_points,
                 combination_randomness,
             });
-            constraints_at_round.push(constraints);
 
             folding_randomness = new_folding_randomness;
 
@@ -220,6 +196,7 @@ where
             domain_size /= 2;
         }
 
+        // In the final round we receive the full polynomial.
         let mut final_coefficients = vec![F::ZERO; 1 << self.params.final_sumcheck_rounds];
         verifier_state.fill_next_scalars(&mut final_coefficients)?;
         let final_coefficients = CoefficientList::new(final_coefficients);
@@ -254,6 +231,7 @@ where
             &final_randomness_answers,
         );
 
+        // Check final in domain evaluations directly
         let final_evaluations = final_coefficients.evaluate_at_univariate(&final_randomness_points);
         if !final_folds
             .iter()
@@ -303,6 +281,25 @@ where
         Ok((folding_randomness, deferred))
     }
 
+    /// Create a random linear combination of constraints and add it to the claim.
+    /// Returns the randomness used.
+    pub fn combine_constraints(
+        &self,
+        verifier_state: &mut VerifierState,
+        claimed_sum: &mut F,
+        constraints: &[Constraint<F>],
+    ) -> ProofResult<Vec<F>> {
+        let [combination_randomness_gen] = verifier_state.challenge_scalars()?;
+        let combination_randomness =
+            expand_randomness(combination_randomness_gen, constraints.len());
+        *claimed_sum += constraints
+            .iter()
+            .zip(&combination_randomness)
+            .map(|(c, rand)| c.sum * rand)
+            .sum::<F>();
+        Ok(combination_randomness)
+    }
+
     /// Verify rounds of sumcheck updating the claimed_sum and returning the folding randomness.
     pub fn verify_sumcheck_rounds(
         &self,
@@ -335,6 +332,56 @@ where
 
         randomness.reverse();
         Ok(MultilinearPoint(randomness))
+    }
+
+    /// Verify a STIR challenges against a commitment and return the constraints.
+    pub fn verify_stir_challenges(
+        &self,
+        verifier_state: &mut VerifierState,
+        params: &StirChallengParams<F>,
+        commitment: &ParsedCommitment<F, MerkleConfig::InnerDigest>,
+        folding_randomness: &MultilinearPoint<F>,
+    ) -> ProofResult<(Vec<Constraint<F>>, Vec<F>)> {
+        let stir_challenges_indexes = get_challenge_stir_queries(
+            params.domain_size,
+            params.folding_factor,
+            params.num_queries,
+            verifier_state,
+        )?;
+
+        let answers =
+            self.verify_merkle_proof(verifier_state, &commitment.root, &stir_challenges_indexes)?;
+
+        self.verify_proof_of_work(verifier_state, params.pow_bits)?;
+
+        // Compute STIR Constraints
+        let folds = self.params.fold_optimisation.stir_evaluations_verifier(
+            self.params,
+            params.round_index,
+            params.domain_gen_inv,
+            &stir_challenges_indexes,
+            folding_randomness,
+            &answers,
+        );
+        let stir_constraints = stir_challenges_indexes
+            .iter()
+            .map(|&index| params.exp_domain_gen.pow([index as u64]))
+            .zip(&folds)
+            .map(|(point, &value)| Constraint {
+                weights: Weights::univariate(point, params.num_variables),
+                sum: value,
+                deferred: false,
+            })
+            .collect();
+
+        // Compute stir points
+        // TODO: This is redundant with constraints
+        let stir_challenges_points = stir_challenges_indexes
+            .iter()
+            .map(|index| params.exp_domain_gen.pow([*index as u64]))
+            .collect();
+
+        Ok((stir_constraints, stir_challenges_points))
     }
 
     /// Verify a merkle multi-opening proof for the provided indices.
