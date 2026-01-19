42fd01da194f3f456bad2778c198af0086150776
Remco Bloemen <remco@wicked.ventures>
2026-01-19 16:37:32 +0100

    Rename NoneEngine strings.

--- src/crypto/proof_of_work/none_engine.rs
+++ src/crypto/proof_of_work/none_engine.rs
@@ -6,7 +6,7 @@ use super::Engine;
 use crate::transcript::{Protocol, ProtocolId};
 
 pub const NONE: ProtocolId = ProtocolId::new(hex!(
-    "3a777695dad25e7b8a5e69f2ecf25e6a0f8927fd334377d297644b14a761dd3d"
+    "c2f551bf887c9a5db24d1da57246e5826ce7ea8bd20e072f5ec12f4616c04a50"
 ));
 
 #[derive(Clone, Copy, PartialEq, Eq, Default)]
@@ -22,7 +22,7 @@ impl Engine for NoneEngine {
     fn check(&self, _challenge: [u8; 32], difficulty: f64, nonce: u64) -> bool {
         assert!(
             difficulty.is_zero(),
-            "Null engine only supports zero difficulty"
+            "None-engine only supports zero difficulty"
         );
         nonce == 0
     }
@@ -30,7 +30,7 @@ impl Engine for NoneEngine {
     fn solve(&self, _challenge: [u8; 32], difficulty: f64) -> Option<u64> {
         assert!(
             difficulty.is_zero(),
-            "Null engine only supports zero difficulty"
+            "None-engine only supports zero difficulty"
         );
         Some(0)
     }
@@ -39,7 +39,7 @@ impl Engine for NoneEngine {
 impl Protocol for NoneEngine {
     fn protocol_id(&self) -> ProtocolId {
         let mut hasher = Sha3_256::new();
-        hasher.update(b"whir::protocols::proof_of_work::NullEngine");
+        hasher.update(b"whir::protocols::proof_of_work::NoneEngine");
         let hash: [u8; 32] = hasher.finalize().into();
         hash.into()
     }
